fd723fbbb4bee7598575c3fca13afeeed4da43fa
Simon Hällström <simonhallstrom@outlook.com>
2020-11-02 12:56:03 +0100

    Add files via upload

---
+++ src/main/java/Fruit.java
@@ -0,0 +1,11 @@
+package se.jensen;
+
+public class Fruit {
+    private String id;
+    private String GetId() {
+        return id;
+    }
+    public void SetId(String newId){
+    this.id = newId;
+    }
+}
